be15bf1dc8199df32522b416cf150ba74574ead9
Ryan Levy <rlevy3@illinois.edu>
2024-12-03 07:30:39 -0500

    Add initial support for automatic selection

--- src/entropy_calc.jl
+++ src/entropy_calc.jl
@@ -1,6 +1,7 @@
 using ITensorMPS: AbstractMPS, orthogonalize, linkinds, siteinds, linkind
 using ITensors: svd, diag, prime, dag, eigen
 
+using ITensors: norm, dim
 include("singular_value_funcs.jl")
 
 function get_ee_bipartite(ψ::AbstractMPS, cut::Int; ee_type=EEType("vN"), kwargs...)::Real
@@ -11,11 +12,11 @@ function get_ee_bipartite(ψ::AbstractMPS, cut::Int; ee_type=EEType("vN"), kwarg
   ψ = orthogonalize(ψ, cut)
   U, S, V = svd(ψ[cut], (linkinds(ψ, cut - 1)..., siteinds(ψ, cut)...))
   Sd = Array(diag(S))
+  Sd = Sd .^ 2 # for entropy calc, we need sᵢ^2
   S_norm = sum(Sd)
   if !(S_norm ≈ 1.0)
     @warn "Normalization of the density matrix isn't 1 (actual=$S_norm)! Be careful!"
   end
-  Sd = Sd .^ 2 # for entropy calc, we need pᵢ^2
   return compute_ee(ee_type, Sd, kwargs...)
 end
 
@@ -73,8 +74,44 @@ function get_density_matrix_bond(ψ_::AbstractMPS, start, stop;)
   return ρ
 end
 
+function get_best_mode(ψ::AbstractMPS, region; verbose=false)
+  """
+    Determine the best mode given a region and MPS
+    if the region is contiguous then determine site vs bond
+    otherwise use the site basis
+
+    TODO: use swap method to force form contiguous
+  """
+  # is this hacky?
+  start, stop = minimum(region), maximum(region)
+  is_contiguous = stop - start + 1 == length(region)
+  (is_contiguous && verbose) &&
+    println("Contiguous region found, considering sites and bond versions")
+  (!is_contiguous && verbose) &&
+    println("No contiguous region found, future version will consider swap")
+  (!is_contiguous) && return "sites"
+
+  # get size of site version
+  # we can always do dim(s[region]) but I'm worried about overflow
+  s = siteinds(ψ)
+  log_sitedim = sum([log2(dim(s[i])) for i in region])
+  (verbose) && println("Site density matrix would be size (log2) $log_sitedim")
+
+  # check that one should really give the inverse region, TODO: modify region based on this
+  log_inverse = sum([log2(dim(si)) for si in s if si ∉ s[region]])
+
+  log_bonddim = log2(dim(linkinds(ψ, start - 1))) + log2(dim(linkinds(ψ, stop)))
+  (verbose) && println("Bond-based density matrix would be size (log2) $log_bonddim")
+  if (log_inverse < log_sitedim) && (log_inverse < log_bonddim)
+    @warn "The compliment of the requested region ($log_inverse) is smaller than the region and the bond version, you should use that instead"
+  end
+  (log_bonddim < log_sitedim) && return "bond"
+
+  return "sites"
+end
+
 function get_ee_region(
-  ψ::AbstractMPS, region; ee_type=EEType("vN"), mode="auto", kwargs...
+  ψ::AbstractMPS, region; ee_type=EEType("vN"), mode="auto", verbose=false, kwargs...
 )::Real
   """
     Get the entanglement entropy of a region of sites, using either the "site" basis
@@ -83,16 +120,15 @@ function get_ee_region(
 
   # TODO: add in calculations to redo sites and mode 
   if mode == "auto"
-    is_contiguous = maximum(region) - minimum(region) + 1 == length(region)
-
-    @warn "auto mode currently defaults to sites"
-    mode = "sites"
+    mode = get_best_mode(ψ, region; verbose)
   end
 
   if mode == "sites"
+    (verbose) && println("Using site mode")
     ρ = get_density_matrix_sites(ψ, region)
   elseif mode == "bond"
     # TODO: make network friendly
+    (verbose) && println("Using bond mode")
     @assert maximum(region) - minimum(region) + 1 == length(region)
     ρ = get_density_matrix_bond(ψ, region[1], region[end])
   end
