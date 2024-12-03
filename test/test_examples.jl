b9871418bfb231800b9162c1572ed65d6b0e9cd9
Ryan Levy <rlevy3@illinois.edu>
2024-12-03 16:57:15 -0500

    Have example tests actually test

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -5,7 +5,8 @@ using Test: @test, @testset
 @testset "Test examples" begin
   example_files = ["basics.jl", "advanced.jl", "rainbow_ghz_w.jl"]
   @testset "Test $example_file" for example_file in example_files
-    @test include(joinpath(pkgdir(ITensorEntropyTools), "examples", example_file)) == nothing
+    @test include(joinpath(pkgdir(ITensorEntropyTools), "examples", example_file)) ==
+      nothing
   end
 end
 end
